89378ff776cb8adeb9fc8efbc34a4a76adf5cd46
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-04-13 13:32:15 +0100

    Updated redart funtion

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const ejs = require("ejs")
-const PORT = 4000
+const PORT = 3000
 
 const app = express()
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 
 app.set("views", "views")
 
 app.get('/', (req, res) =>{
-    console.log('index')
+    // console.log('index')
     res.render('index')
 })
 
+app.post('/redact', (req, res) =>{
+   try {
+    const {sentence, words, replace} = req.body
+
+    res.render('redart', { redactedContent: words, originalContent:sentence });
+
+   } catch (error) {
+    
+   }
+})
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
