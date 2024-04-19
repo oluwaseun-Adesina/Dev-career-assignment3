7ebd00590e6ffe6c2d628f46a69143a913bae573
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2024-04-19 11:46:13 +0100

    Update te redart function and fixed bug

--- app.js
+++ app.js
@@ -15,15 +15,65 @@ app.get('/', (req, res) =>{
 })
 
 app.post('/redact', (req, res) =>{
-   try {
-    const {sentence, words, replace} = req.body
+    try {
+        // start counting Performance time
+        const startTime = performance.now();
+        const { sentence, words, placeholder } = req.body;
 
-    res.render('redart', { redactedContent: words, originalContent:sentence });
+        // Implement scramble logic
+        let replace;
+        if (placeholder === 'asterisks') {
+            replace = "****";
+        } else if (placeholder === 'questionMark') {
+            replace = "????";
+        } else if (placeholder === 'dashes') {
+            replace = "----";
+        }
 
-   } catch (error) {
-    
-   }
-})
+        // Split the words string into an array of individual words
+        const wordsToReplace = words.split(' ');
+
+        // Replace each word individually using a loop
+        let redactedContent = sentence;
+        wordsToReplace.forEach(word => {
+            redactedContent = redactedContent.split(word).join(replace);
+        });
+
+        // Calculate statistics
+        const wordsScanned = sentence.split(/\s+/).length;
+
+        let wordsRedacted = 0;
+        wordsToReplace.forEach(word => {
+            wordsRedacted += (sentence.split(word).length - 1);
+        });
+
+        const charactersRedacted = redactedContent.length - sentence.length;
+
+        // Get performance time
+        const endTime = performance.now();
+        let timeTaken = endTime - startTime;
+        // Truncate the timeTaken to 3 decimal places
+        timeTaken = timeTaken.toPrecision(3);
+
+        // Render the view with the redacted content and statistics
+        res.render('redart', {
+            redactedContent,
+            originalContent: sentence,
+            stats: {
+                wordsScanned,
+                wordsRedacted,
+                charactersRedacted,
+                timeTaken
+            }
+        });
+    } catch (error) {
+        // Handle errors appropriately
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
